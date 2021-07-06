c0662a8bd8b2e1279ad6ba5f963e6a1e6941452a
Nina Shirole <ninashirole@uchicago.edu>
2021-07-06 04:18:54 +0000

    letter count

--- each_letter_count.rb
+++ each_letter_count.rb
@@ -2,5 +2,16 @@
 # 
 # Asks the user to enter a word.
 #  The program should print each letter in the word the number of times it appears in the word.
+#"Enter a word"
+#loop
+#l appears 1 times
+#o appears 2 times
+#o appears 2 times
+#p appears 1 times#
 
 p "Enter a word:"
+word = gets.chomp
+wordsplit = word.split("")
+wordsplit.each do |the_letter|
+    p "#{the_letter} appears #{wordsplit.count(the_letter)} times"
+  end
