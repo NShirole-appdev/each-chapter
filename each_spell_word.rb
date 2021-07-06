b4df74b7d7dd4cd357a0e40f56f9d8df86eaddd2
Nina Shirole <ninashirole@uchicago.edu>
2021-07-06 04:11:32 +0000

    spell word

--- each_spell_word.rb
+++ each_spell_word.rb
@@ -3,4 +3,10 @@
 # Asks your user to enter any word and have it spelled out letter by letter.  
 
 p "Enter a word for me to spell:"
+word = gets.chomp
+word_array = []
+word_array = word.split("")
+word_array.each do |the_letter|
+    p the_letter
+  end
 
